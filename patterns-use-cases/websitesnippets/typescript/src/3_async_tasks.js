7b3525de19c0764db9bed07bfa69917ef59fd2eb
Stephan Ewen <stephan@restate.dev>
2024-05-30 12:44:46 +0200

    rework async task example

--- patterns-use-cases/websitesnippets/typescript/src/3_async_tasks.js
+++ patterns-use-cases/websitesnippets/typescript/src/3_async_tasks.js
@@ -3,25 +3,35 @@ const clients = require ("@restatedev/restate-sdk-clients");
 
 // *** BEGIN SNIPPET ***
 
-// --- service (= worker) ---
+// ------ service (= worker) ------
 const asyncTaskService = restate.service({
     name: "taskWorker",
-    handlers: {
-        runTask: async (ctx, params) => {
-            return someHeavyWork(params);
-        }
-    }
+    handlers: { processPayment }
 });
 
-// --- client ---
+// ------ client ------
 const rs = clients.connect({ url: process.env.RESTATE_URL });
-const taskHandle = await rs
-    .serviceSendClient({ name: "taskWorker" })
-    .runTask(
-        { /* task params */ },
-        SendOpts.from({ idempotencyKey: "dQw4w9WgXcQ" })
-    );
+const taskWorker = rs.serviceSendClient({ name: "taskWorker" });
 
-const result = await rs.result(taskHandle);
+// submit the payment task 
+app.post('/charge/:paymentId', async (req, res) => {
+    const taskHandle = await taskWorker.processPayment(
+        { request: req.params },
+        SendOpts.from({ idempotencyKey: req.params.paymentId })
+    );
+    
+    res.json(taskHandle);
+});
+
+// await the payment task
+app.get('/status', async (req,res) => {
+        const taskHandle = req.body.json();
+        const paymentResult = await restate.result(taskHandle); 
+        res.join(paymentResult);
+});
 
 // *** END SNIPPET ***
+
+// ----------------------- Stubs  -----------------------
+
+async function processPayment(paymentDetails) {}
